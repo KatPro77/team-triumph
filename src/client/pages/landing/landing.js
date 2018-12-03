9387b4953aa63bd51d33c3f1f758ec5f24b1627c
JBurton25 <jordonalexander@gmail.com>
2018-12-03 14:31:34 -0500

    added modal / complex buttons / file structure for search

--- src/client/pages/landing/landing.js
+++ src/client/pages/landing/landing.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import './landing.css';
 import Navbar from '../../components/navbar/navbar.js'
-import Carousel from '../../components/carousel/carousel.js'
+// import Carousel from '../../components/carousel/carousel.js'
+import ButtonBases from '../../components/complexbutton/complexbutton.js'
+import Button from '@material-ui/core/Button';
+import SimpleModalWrapped from '../../components/modal/modal.js'
 
-export default class Landing extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Carousel />
-      </div>
-    );
-  }
-}
+
+
+
+// export default class Landing extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Navbar />
+//         <Carousel />
+//       </div>
+//     );
+//   }
+// }
+
+class Cams extends Component {
+h
+andleOpen = () => {
+   this.setState({ open: true });
+       };
+ handleClose = () => {
+ this.setState({ open: false });
+       };
+   render() {
+     return (
+        <div>
+            <Navbar />
+            <SimpleModalWrapped />
+            <ButtonBases onClick={this.handleOpen}/>
+        </div>
+     );
+   }
+ }
+ 
+ export default Cams;
 
